ec179f0d389373b567fe2a60fc901aee2e983b7d
Eduardo Branquinho <edubranquinho@gmail.com>
2016-07-04 23:59:04 -0300

    iniciando test no /post

---
+++ src/test/java/br/com/treld/TestUsingWebAppContextSetup.java
@@ -0,0 +1,37 @@
+package br.com.treld;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * Created by edubranquinho on 29/07/16.
+ */
+public class TestUsingWebAppContextSetup extends TreldBlogApplicationTests {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void validatePost() throws Exception {
+		ResultActions resultado = mockMvc.perform(post("/login").contentType(MediaType.APPLICATION_FORM_URLENCODED).param("username", "treld")
+				.param("password", "treld")).andExpect(status().isOk());
+		System.out.println(resultado);
+	}
+
+}
