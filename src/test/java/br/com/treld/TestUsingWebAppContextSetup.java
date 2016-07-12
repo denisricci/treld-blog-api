06802accb98770ffad0afae97050d0db427ae88a
Renã Passos de Souza <ren.boxe@gmail.com>
2016-07-11 22:19:08 -0300

    FIX Auth test

--- src/test/java/br/com/treld/TestUsingWebAppContextSetup.java
+++ src/test/java/br/com/treld/TestUsingWebAppContextSetup.java
@@ -1,40 +1,45 @@
 package br.com.treld;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.Filter;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by edubranquinho on 29/07/16.
  */
 @TreldTest
 @RunWith(SpringJUnit4ClassRunner.class)
-public class TestUsingWebAppContextSetup extends TreldBlogApplicationTests {
+public class TestUsingWebAppContextSetup {
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
+
+	@Autowired
+	private Filter springSecurityFilterChain;
 
 	private MockMvc mockMvc;
 
 	@Before
 	public void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).addFilter(springSecurityFilterChain).build();
 	}
 
 	@Test
 	public void validatePost() throws Exception {
-		ResultActions resultado = mockMvc.perform(post("/login").param("username", "treld")
-				.param("password", "treld")).andExpect(status().isOk());
-		System.out.println(resultado);
+		mockMvc.perform(post("/login")
+							.param("username", "treld")
+							.param("password", "treld"))
+						.andExpect(status().isOk());
 	}
 
 }
