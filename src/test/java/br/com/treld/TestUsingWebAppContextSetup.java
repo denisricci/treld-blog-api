1c1abe5a110e6693491d43c5d98460de993100d9
Eduardo Branquinho <edubranquinho@gmail.com>
2016-07-13 21:38:26 -0300

    testing url from post

--- src/test/java/br/com/treld/TestUsingWebAppContextSetup.java
+++ src/test/java/br/com/treld/TestUsingWebAppContextSetup.java
@@ -1,18 +1,24 @@
 package br.com.treld;
 
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.servlet.Filter;
+import javax.servlet.http.HttpSession;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import javax.servlet.Filter;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by edubranquinho on 29/07/16.
@@ -28,18 +34,36 @@ public class TestUsingWebAppContextSetup {
 	private Filter springSecurityFilterChain;
 
 	private MockMvc mockMvc;
+	private HttpSession session;
 
 	@Before
-	public void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).addFilter(springSecurityFilterChain).build();
+	public void setup() throws Exception {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).addFilter(springSecurityFilterChain)
+				.build();
+		login();
 	}
 
+	public void login() throws Exception {
+		this.session = mockMvc.perform(post("/login").param("username", "treld").param("password", "treld")).andReturn()
+				.getRequest().getSession();
+	}
+
+	/*
+	 * This test validates the URI that is generated when a post is created. It
+	 * also validates if the post was really created.
+	 * 
+	 * @Author: Eduardo
+	 */
 	@Test
-	public void validatePost() throws Exception {
-		mockMvc.perform(post("/login")
-							.param("username", "treld")
-							.param("password", "treld"))
-						.andExpect(status().isOk());
+	public void validateUriPost() throws Exception {
+		String post = " { \"title\":\"Creating rest api\", \"body\":\"open your ide...\" } ";
+		ResultActions result = mockMvc.perform(post("/api/post/").content(post).contentType(MediaType.APPLICATION_JSON)
+				.session((MockHttpSession) session)).andExpect(status().isCreated());
+		System.out.println(result.andReturn().getResponse().getRedirectedUrl());
+		String localResourceCreated = result.andReturn().getResponse().getRedirectedUrl();
+		ResultActions resultSearch = mockMvc.perform(get(localResourceCreated).session((MockHttpSession) session));
+		String responseString = resultSearch.andReturn().getResponse().getContentAsString();
+		assertTrue(responseString.contains("Creating rest api"));
 	}
 
 }
